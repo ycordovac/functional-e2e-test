ee59be0d48d3dca77e405f4eac1711bf2b37f912
fran.aznar <francisco.aznar@nexoqa.com>
2022-10-20 10:42:52 +0200

    fix test

--- src/main/java/com/bootcamp/tasks/AddTutorial.java
+++ src/main/java/com/bootcamp/tasks/AddTutorial.java
@@ -16,7 +16,7 @@ public class AddTutorial implements Task {
     @Override
     @Step("{0} navigate to the form section")
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(Open.relativeUrl("http://localhost:80"));
+        actor.attemptsTo(Open.relativeUrl("http://angular-14-app.default:80"));
         actor.attemptsTo(FillAddTutorialForm.fill());
     }
 
