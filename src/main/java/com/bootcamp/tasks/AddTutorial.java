0e5f565b447dc79cfb5a54649f1fdbf8ab215aa4
yandi <ycordova@devcenter.es>
2022-11-21 14:10:22 +0100

    exponiendo Puerto1

--- src/main/java/com/bootcamp/tasks/AddTutorial.java
+++ src/main/java/com/bootcamp/tasks/AddTutorial.java
@@ -16,7 +16,7 @@ public class AddTutorial implements Task {
     @Override
     @Step("{0} navigate to the form section")
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(Open.relativeUrl("https://vast-beers-grab-213-0-57-163.loca.lt:80"));
+        actor.attemptsTo(Open.relativeUrl("https://vast-beers-grab-213-0-57-163.loca.lt/tutorials"));
         actor.attemptsTo(FillAddTutorialForm.fill());
     }
 
